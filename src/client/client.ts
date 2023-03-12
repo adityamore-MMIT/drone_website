3c9a3d7ce446185c1e1a7f661557cdb0a9752a2f
Aditya <aditya.more@mmit.edu.in>
2023-03-12 16:34:48 +0000

    Update 5 files

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,76 +1,64 @@
-import * as THREE from 'three'
-import Stats from 'three/examples/jsm/libs/stats.module'
-import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
+import * as THREE from "three";
+import Stats from "three/examples/jsm/libs/stats.module";
+// import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 
-const gridHelper = new THREE.GridHelper(10, 10, 0xaec6cf, 0xaec6cf)
-scene.add(gridHelper)
+const gridHelper = new THREE.GridHelper(10, 10, 0xaec6cf, 0xaec6cf);
+scene.add(gridHelper);
 
 const camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-)
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
 
-const renderer = new THREE.WebGLRenderer()
-renderer.setSize(window.innerWidth, window.innerHeight)
-document.body.appendChild(renderer.domElement)
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
 // reference to drone model
 let loadedObject;
 
+// fetch from localstorage
 
-// instance of loader
-let loader = new THREE.ObjectLoader();
+const loader = new GLTFLoader();
 
-// load the drone model
+// loader.setPath("/project/home/adityamore-mmit/workspace/");
+//gltf model
 loader.load(
-
-    './drone.obj',
-    function (object) {
-        console.log('Object loaded successfully!');
-        scene.add(object);
-        // object.position.set(0, 0, 0);
-
-        // ----------center the object---------
-
-        // Compute the bounding box of the object
-        const bbox = new THREE.Box3().setFromObject(object);
-        
-        // Compute the center of the bounding box
-        const center = new THREE.Vector3();
-        bbox.getCenter(center);
-        // -----------------------------------
-
-        // Move the object so its center is at the origin
-        // object.position.sub(center);
-        object.position.set(0,0,0);
-        
-        // scale down
-        object.scale.set(0.4,0.4,0.4);
-
-        loadedObject = object;
-    }
+  "/project/home/adityamore-mmit/workspace/drone.gltf",
+  (gltf) => {
+    // Called when the model is loaded
+    scene.add(gltf.scene);
+  },
+  (xhr) => {
+    // Called while the model is loading
+    console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+  },
+  (error) => {
+    // Called if there is an error loading the model
+    console.error("Error loading GLTF model", error);
+  }
 );
 
-
 const material = new THREE.MeshBasicMaterial({
-    color: 0x00ff00,
-    wireframe: true,
-})
+  color: 0x00ff00,
+  wireframe: true,
+});
 
-const drone = new THREE.Mesh(loadedObject, material)
-drone.position.set(0, 0.5, -10)
-scene.add(drone)
+const drone = new THREE.Mesh(loadedObject, material);
+drone.position.set(0, 0.5, -10);
+scene.add(drone);
 
-window.addEventListener('resize', onWindowResize, false)
+window.addEventListener("resize", onWindowResize, false);
 function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight
-    camera.updateProjectionMatrix()
-    renderer.setSize(window.innerWidth, window.innerHeight)
-    render()
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  render();
 }
 
 /* Liner Interpolation
@@ -82,15 +70,15 @@ function onWindowResize() {
  * lerp(-20, -10, .1)) = -.19
  */
 function lerp(x: number, y: number, a: number): number {
-    return (1 - a) * x + a * y
+  return (1 - a) * x + a * y;
 }
 
 // Used to fit the lerps to start and end at specific scrolling percentages
 function scalePercent(start: number, end: number) {
-    return (scrollPercent - start) / (end - start)
+  return (scrollPercent - start) / (end - start);
 }
 
-const animationScripts: { start: number; end: number; func: () => void }[] = []
+const animationScripts: { start: number; end: number; func: () => void }[] = [];
 
 //add an animation that flashes the drone through 100 percent of scroll
 // animationScripts.push({
@@ -108,89 +96,88 @@ const animationScripts: { start: number; end: number; func: () => void }[] = []
 
 //add an animation that moves the drone through first 40 percent of scroll
 animationScripts.push({
-    start: 0,
-    end: 40,
-    func: () => {
-        camera.lookAt(drone.position)
-        camera.position.set(0, 1, 2)
-        drone.position.z = lerp(-10, 0, scalePercent(0, 40))
-        //console.log(drone.position.z)
-    },
-})
+  start: 0,
+  end: 40,
+  func: () => {
+    camera.lookAt(drone.position);
+    camera.position.set(0, 1, 2);
+    drone.position.z = lerp(-10, 0, scalePercent(0, 40));
+    //console.log(drone.position.z)
+  },
+});
 
 //add an animation that rotates the drone between 40-60 percent of scroll
 animationScripts.push({
-    start: 40,
-    end: 60,
-    func: () => {
-        camera.lookAt(drone.position)
-        camera.position.set(0, 1, 2)
-        drone.rotation.z = lerp(0, Math.PI, scalePercent(40, 60))
-        //console.log(drone.rotation.z)
-    },
-})
+  start: 40,
+  end: 60,
+  func: () => {
+    camera.lookAt(drone.position);
+    camera.position.set(0, 1, 2);
+    drone.rotation.z = lerp(0, Math.PI, scalePercent(40, 60));
+    //console.log(drone.rotation.z)
+  },
+});
 
 //add an animation that moves the camera between 60-80 percent of scroll
 animationScripts.push({
-    start: 60,
-    end: 80,
-    func: () => {
-        camera.position.x = lerp(0, 5, scalePercent(60, 80))
-        camera.position.y = lerp(1, 5, scalePercent(60, 80))
-        camera.lookAt(drone.position)
-        //console.log(camera.position.x + " " + camera.position.y)
-    },
-})
+  start: 60,
+  end: 80,
+  func: () => {
+    camera.position.x = lerp(0, 5, scalePercent(60, 80));
+    camera.position.y = lerp(1, 5, scalePercent(60, 80));
+    camera.lookAt(drone.position);
+    //console.log(camera.position.x + " " + camera.position.y)
+  },
+});
 
 //add an animation that auto rotates the drone from 80 percent of scroll
 animationScripts.push({
-    start: 80,
-    end: 101,
-    func: () => {
-        //auto rotate
-        drone.rotation.x += 0.01
-        drone.rotation.y += 0.01
-    },
-})
+  start: 80,
+  end: 101,
+  func: () => {
+    //auto rotate
+    drone.rotation.x += 0.01;
+    drone.rotation.y += 0.01;
+  },
+});
 
 function playScrollAnimations() {
-    animationScripts.forEach((a) => {
-        if (scrollPercent >= a.start && scrollPercent < a.end) {
-            a.func()
-        }
-    })
+  animationScripts.forEach((a) => {
+    if (scrollPercent >= a.start && scrollPercent < a.end) {
+      a.func();
+    }
+  });
 }
 
-let scrollPercent = 0
+let scrollPercent = 0;
 
 document.body.onscroll = () => {
-    //calculate the current scroll progress as a percentage
-    scrollPercent =
-        ((document.documentElement.scrollTop || document.body.scrollTop) /
-            ((document.documentElement.scrollHeight ||
-                document.body.scrollHeight) -
-                document.documentElement.clientHeight)) *
-        100
-    ;(document.getElementById('scrollProgress') as HTMLDivElement).innerText =
-        'Scroll Progress : ' + scrollPercent.toFixed(2)
-}
+  //calculate the current scroll progress as a percentage
+  scrollPercent =
+    ((document.documentElement.scrollTop || document.body.scrollTop) /
+      ((document.documentElement.scrollHeight || document.body.scrollHeight) -
+        document.documentElement.clientHeight)) *
+    100;
+  (document.getElementById("scrollProgress") as HTMLDivElement).innerText =
+    "Scroll Progress : " + scrollPercent.toFixed(2);
+};
 
-const stats = Stats()
-document.body.appendChild(stats.dom)
+const stats = Stats();
+document.body.appendChild(stats.dom);
 
 function animate() {
-    requestAnimationFrame(animate)
+  requestAnimationFrame(animate);
 
-    playScrollAnimations()
+  playScrollAnimations();
 
-    render()
+  render();
 
-    stats.update()
+  stats.update();
 }
 
 function render() {
-    renderer.render(scene, camera)
+  renderer.render(scene, camera);
 }
 
-window.scrollTo({ top: 0, behavior: 'smooth' })
-animate()
+window.scrollTo({ top: 0, behavior: "smooth" });
+animate();
